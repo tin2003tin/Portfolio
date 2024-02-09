29e78680d93c58a15193d7a2418758d3fd6437eb
tin2003tin Thongon <tinsiriwid@gmail.com>
2024-02-09 13:55:38 +0000

    add env

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,18 +1,17 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDBJzpYdXY6AtX4-j-fp7Z_LDWrKVdY9f0",
-    authDomain: "portfolio-tintin-72afe.firebaseapp.com",
-    projectId: "portfolio-tintin-72afe",
-    storageBucket: "portfolio-tintin-72afe.appspot.com",
-    messagingSenderId: "651948637636",
-    appId: "1:651948637636:web:14d007b16f95a777603891",
-    measurementId: "G-3P8CGNRHD7"
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID
   };
 
 const app = initializeApp(firebaseConfig);
